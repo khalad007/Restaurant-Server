f14e4b706ee4c66bc35e1e2bd0cfcd741dd894d1
khalad007 <www.arifhossain49919@gmail.com>
2023-11-16 22:03:48 -0800

    Get , post , delete operation for cart

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pcnyajy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,15 +30,37 @@ async function run() {
 
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewCollection = client.db("bistroDB").collection("reviews")
+    const cartCollection = client.db("bistroDB").collection("carts")
 
     app.get('/menu', async (req, res) => {
-        const result = await menuCollection.find().toArray();
-        res.send(result)
+      const result = await menuCollection.find().toArray();
+      res.send(result)
     })
 
-    app.get('/review', async(req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result)
+    })
+
+    //carts collection 
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result)
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
@@ -54,9 +76,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('rasturant is running')
+  res.send('rasturant is running')
 })
 
 app.listen(port, () => {
-    console.log(`Rasturant douwrd er upor ache ${port}`)
+  console.log(`Rasturant douwrd er upor ache ${port}`)
 })
